25e0eca9b3b4dd626467a5f92e9728f6f5899037
Giuseppe D'Addario <daddario.2177530@studenti.uniroma1.it>
2025-06-11 09:23:23 +0200

    Few modifications

--- lib/roboticsII/pinv_w.m
+++ lib/roboticsII/pinv_w.m
@@ -1,4 +1,9 @@
 function J_pinv = pinv_w(J,W)
     disp("The weighted pseudoinverse formula: W^-1*J'*(J*W^-1*J')^-1")
-    J_pinv = simplify(W^-1*J.'*(J*W^-1*J.')^-1);
+    if isa(J,'sym')
+        J_pinv = simplify(W^-1*J.'*(J*W^-1*J.')^-1);
+    else
+        J_pinv = W^-1*J.'*(J*W^-1*J.')^-1;
+    end
+
 end
